963bacdf84b258da3f8a50654a1f017581549ba4
Parekh <aparekh@ptc.com>
2019-09-24 12:48:19 +0530

    Retrieve logged in user details.

--- security-oauth/ui/src/main/java/com/example/ui/SocialApplication.java
+++ security-oauth/ui/src/main/java/com/example/ui/SocialApplication.java
@@ -1,15 +1,25 @@
 package com.example.ui;
 
+import java.security.Principal;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableOAuth2Sso
+@RestController
 public class SocialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialApplication.class, args);
+	}
+	
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
 	}
 
 }
