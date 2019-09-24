f5de6ac725ee6d59652c0b5e4a47829197378026
Parekh <aparekh@ptc.com>
2019-09-24 11:54:37 +0530

    Authentication using facebook - Simple example

--- security-oauth/ui/src/main/java/com/example/ui/SocialApplication.java
+++ security-oauth/ui/src/main/java/com/example/ui/SocialApplication.java
@@ -2,12 +2,14 @@ package com.example.ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
-public class UiApplication {
+@EnableOAuth2Sso
+public class SocialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UiApplication.class, args);
+		SpringApplication.run(SocialApplication.class, args);
 	}
 
 }
